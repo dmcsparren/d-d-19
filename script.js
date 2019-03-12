24034f669b7d7bdaf56eea73298c89d189214292
dmcsparren <39099735+dmcsparren@users.noreply.github.com>
2019-03-12 11:42:14 -0400

    Initial Upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function roll() {
+    var dice = prompt("How many sides are you rolling?");
+    console.log(dice);
+
+    function getRoll(max) {
+        return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+    alert("You rolled a " + getRoll(dice));
+}
+
+// this function works
+// function roll() {
+//     var dice = prompt("How many sides are you rolling?");
+//     console.log(dice);
+// }
+
+function roll20() {
+    var dice = 19;
+    console.log(dice);
+
+    function getRoll(max) {
+        return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+    alert("You rolled a " + getRoll(dice));
+    console.log(dice);
+}
+
+function roll12() {
+    var dice = 11;
+    console.log(dice);
+
+    function getRoll(max) {
+        return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+    alert("You rolled a " + getRoll(dice));
+}
+
+function roll10() {
+    var dice = 9;
+    console.log(dice);
+
+    function getRoll(max) {
+        return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+    alert("You rolled a " + getRoll(dice));
+}
+
+function roll8() {
+    var dice = 7;
+    console.log(dice);
+
+    function getRoll(max) {
+        return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+    alert("You rolled a " + getRoll(dice));
+}
+
+function roll6() {
+    var dice = 5;
+    console.log(dice);
+
+    function getRoll(max) {
+        return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+    console.log(dice);
+    alert("You rolled a " + getRoll(dice));
+}
